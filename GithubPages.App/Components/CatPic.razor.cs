7a4c2fc056a35b31b2e65b55c3c425d4ca21bac5
Alan Zelaquett Hoffmann <alan.zh@hotmail.com>
2024-04-28 04:09:20 +0100

    Added cat pics

---
+++ GithubPages.App/Components/CatPic.razor.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Json;
+using GithubPages.App.Components.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace GithubPages.App.Components;
+
+public partial class CatPic
+{
+    private const string BaseUrl = "https://cataas.com/cat";
+
+    public int Width { get; set; } = 400;
+    public int Height { get; set; } = 300;
+
+    [Inject]
+    private HttpClient Client { get; set; } = default!;
+
+    [Parameter]
+    public string? Text { get; set; }
+
+    [MemberNotNullWhen(true, nameof(CatPicUrl))]
+    public bool HasCatPic { get; set; }
+
+    public string? CatPicUrl { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        var cat = await Client.GetFromJsonAsync<Cat>(BaseUrl + "?json=true");
+
+        if (cat is not null)
+        {
+            HasCatPic = true;
+            CatPicUrl = $"{BaseUrl}/{cat.Id}?width={Width}&height={Height}";
+
+            Console.WriteLine(CatPicUrl);
+        }
+    }
+}
---
+++ GithubPages.App/Components/Models/Cat.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GithubPages.App.Components.Models;
+
+public class Cat
+{
+    [JsonPropertyName("_id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("tags")]
+    public string[] Tags { get; set; }
+
+    [JsonPropertyName("mimetype")]
+    public string MimeType { get; set; }
+
+    [JsonPropertyName("size")]
+    public int Size { get; set; }
+
+    [JsonPropertyName("createdAt")]
+    public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("editedAt")]
+    public DateTime EditedAt { get; set; }
+
+}
